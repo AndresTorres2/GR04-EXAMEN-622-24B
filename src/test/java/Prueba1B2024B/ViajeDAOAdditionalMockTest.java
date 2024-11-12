4d31653ac41b40843721267a3c9a1135ea0cf4dc
TuT-Dem <andreshernandez20001@gmail.com>
2024-11-12 10:40:56 -0500

    Comentar pruebas unitarias y mock, no usadas y norelacionadas con la BD.

--- src/test/java/Prueba1B2024B/ViajeDAOAdditionalMockTest.java
+++ src/test/java/Prueba1B2024B/ViajeDAOAdditionalMockTest.java
@@ -18,7 +18,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 import static org.mockito.Mockito.*;
-        import static org.junit.Assert.*;
+import static org.junit.Assert.*;
 
 @RunWith(MockitoJUnitRunner.class)
 public class ViajeDAOAdditionalMockTest {
